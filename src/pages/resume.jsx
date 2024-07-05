5381d2b2dd3dc0be952b8f86c8b2088599399a1f
Caleb <2021.cee.jay@gmail.com>
2024-07-05 20:42:37 +0100

    first commit

---
+++ src/pages/resume.jsx
@@ -0,0 +1,9 @@
+const Resume = () => {
+    return (
+        <div className=" text-center mt-5">
+            <img src="/calebResume.PNG" alt="caleb's resume" className="" />
+        </div>
+    );
+}
+ 
+export default Resume;
